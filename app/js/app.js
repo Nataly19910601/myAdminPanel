cc249b6bf048a390983f05b20bd36400409130ce
Nataly <natashaka91@gmail.com>
2017-11-03 13:07:04 +0700

    three

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
-angular.module('admin', ['admin.services','admin.filters'])
+var myadminApp =angular.module('admin', ['admin.services','admin.filters'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
-            .when('/list', {template: 'views/list.html', controller: ListCtrl})
-            .when('/new', {template: 'views/edit.html', controller: NewCtrl})
-            .when('/edit/:id', {template: 'views/edit.html', controller: EditCtrl})
-            .otherwise({redirectTo: '/list'});
+            .when('/', {template: 'template/index.html', controller: defaultCtrl})
+            .when('/tables', {template: 'template/tables.html', controller: NewCtrl})
+            .when('/diagrams/:id', {template: 'template/diagrams.html', controller: EditCtrl})
+            .otherwise({redirectTo: '/'});
     },
     ]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,110 +1,36 @@
 'use strict';
 
+var myadminApp =angular.module('admin', ['admin.services','admin.filters'])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider
+            .when('/', {template: 'template/index.html', controller: defaultCtrl})
+            .when('/tables', {template: 'template/tables.html', controller: NewCtrl})
+            .when('/diagrams/:id', {template: 'template/diagrams.html', controller: EditCtrl})
+            .otherwise({redirectTo: '/'});
+    },
+    ]);
 /* Controllers */
-var phonecatApp = angular.module('phonecatApp', ['ngRoute', 'ngResource']);
 
-/* Config */
-phonecatApp.config([
-  '$routeProvider', '$locationProvider',
-  function($routeProvide, $locationProvider){
-    $routeProvide
-        .when('/',{
-          templateUrl:'template/home.html',
-          controller:'PhoneListCtrl'
-        })
-        .when('/about',{
-          templateUrl:'template/about.html',
-          controller:'AboutCtrl'
-        })
-        .when('/contact',{
-          templateUrl:'template/contact.html',
-          controller:'ContactCtrl'
-        })
-        .when('/phones/:phoneId', {
-          templateUrl:'template/phone-detail.html',
-          controller:'PhoneDetailCtrl'
-        })
-        .otherwise({
-          redirectTo: '/'
+
+
+myadminApp.controller('DefaultCtrl',[
+    '$scope','$http', '$location', 'Phone',
+    function($scope, $http, $location, Phone) {
+
+        Phone.query({phoneId: 'phones'}, function(data) {
+            $scope.phones = data;
         });
-  }
-]);
 
-/* Factory */
-phonecatApp.factory('Phone', [
-  '$resource', function($resource) {
-    return $resource('phones/:phoneId.:format', {
-      phoneId: 'phones',
-      format: 'json',
-      apiKey: 'someKeyThis'
-      /* http://localhost:8888/phones/phones.json?apiKey=someKeyThis */
-    }, {
-      // action: {method: <?>, params: <?>, isArray: <?>, ...}
-      update: {method: 'PUT', params: {phoneId: '@phone'}, isArray: true}
-    });
-    //Phone.update(params, successcb, errorcb);
-  }
-]);
 
-/* Filter */
-phonecatApp.filter('checkmark', function() {
-  return function(input) {
-    return input ? '\u2713' : '\u2718';
-  }
-});
 
-phonecatApp.controller('PhoneListCtrl',[
-  '$scope','$http', '$location', 'Phone',
-  function($scope, $http, $location, Phone) {
-
-    Phone.query({phoneId: 'phones'}, function(data) {
-      $scope.phones = data;
-    });
-
-    //Phone.query(params, successcb, errorcb)
-
-    //Phone.get(params, successcb, errorcb)
-
-    //Phone.save(params, payloadData, successcb, errorcb)
-
-    //Phone.delete(params, successcb, errorcb)
-
-  }
-]);
-
-/* About Controller */
-phonecatApp.controller('AboutCtrl',[
-  '$scope','$http', '$location',
-  function($scope, $http, $location) {
-
-  }
-]);
-
-/* Contact Controller */
-phonecatApp.controller('ContactCtrl',[
-  '$scope','$http', '$location',
-  function($scope, $http, $location) {
-
-  }
-]);
-
-/* Phone Detail Controller */
-phonecatApp.controller('PhoneDetailCtrl',[
-  '$scope','$http', '$location', '$routeParams', 'Phone',
-  function($scope, $http, $location, $routeParams, Phone) {
-    $scope.phoneId = $routeParams.phoneId;
-
-    Phone.get({phoneId: $routeParams.phoneId}, function(data) {
-      $scope.phone = data;
-      $scope.mainImageUrl = data.images[0];
-      //data.$save();
-    });
-
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
     }
-
-  }
 ]);
+
+
+
+
+
+
+
 
 
